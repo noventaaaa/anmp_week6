b31cc49f1b64f68976c4a5ae4afa0de5d45500ae
noventaaaa <107040216+noventaaaa@users.noreply.github.com>
2024-03-21 02:22:49 +0700

    card

---
+++ app/src/main/java/com/example/advweek4_160420042/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.advweek4_160420042.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.advweek4_160420042.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
