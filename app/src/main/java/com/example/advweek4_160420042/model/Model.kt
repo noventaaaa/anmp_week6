ca654eb0f5f2cd51b4bbff88adbe2dd96808a970
noventaaaa <107040216+noventaaaa@users.noreply.github.com>
2024-03-21 13:31:39 +0700

    .

---
+++ app/src/main/java/com/example/advweek4_160420042/model/Model.kt
@@ -0,0 +1,9 @@
+package com.example.advweek4_160420042.model
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
--- app/src/main/java/com/example/advweek4_160420042/view/StudentListFragment.kt
+++ app/src/main/java/com/example/advweek4_160420042/view/StudentListFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.advweek4_160420042.R
+import com.example.advweek4_160420042.databinding.FragmentStudentListBinding
+import com.example.advweek4_160420042.viewmodel.ListViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,24 +23,52 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StudentListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+    private lateinit var binding: FragmentStudentListBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = studentListAdapter
+        observeViewModel()
+
+    }
+
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            studentListAdapter.updateStudentList(it)
+        })
+
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.txtError?.visibility = View.VISIBLE
+            } else {
+                binding.txtError?.visibility = View.GONE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_list, container, false)
+        binding = FragmentStudentListBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
     companion object {
